0f18899c21c9f7c189c46e6a93f37376d31de945
PioneerX7 <vita-kopy@yandex.ru>
2023-03-22 21:43:22 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-// Допустим, у нас есть. информация о заказе, которая записана :примерно в таком формате.
+// Допустим, у нас есть. информация о о заказе, которая записана :примерно в таком формате.
 
 public class Main {
     public static void main(String[] args) {
@@ -12,19 +14,23 @@ public class Main {
 
     }
     public static void clean(String text){
+        Map<String, Integer> map = new TreeMap<>();
         List<String> list = List.of(text.replace(",", "")
                 .replace(".", "")
                 .replace(":", "")
                 .replace("(", "")
                 .replace(")", "")
                 .split(" "));
-        list.forEach(System.out::println);
-        Set<String> set = new HashSet<>();
-        Integer i = 1;
-        for (String x : list) {
-            i++;
-            set.add(x);
+        list.forEach(x -> {
+            if(map.containsKey(x)){
+                map.put(x, map.get(x) + 1);
+            } else {
+                map.put(x, 1);
+            }
+        });
+
         }
-        System.out.println(set);
+        public static void sorted(){
+
+        }
     }
-}
