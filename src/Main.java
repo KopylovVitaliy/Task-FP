ca5ebf6262c597328aacb5471ecda804020ab6e8
PioneerX7 <vita-kopy@yandex.ru>
2023-03-22 19:56:18 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.*;
+
+// Допустим, у нас есть. информация о заказе, которая записана :примерно в таком формате.
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите текст:");
+        String text = scanner.nextLine();
+        clean(text);
+
+    }
+    public static void clean(String text){
+        List<String> list = List.of(text.replace(",", "")
+                .replace(".", "")
+                .replace(":", "")
+                .replace("(", "")
+                .replace(")", "")
+                .split(" "));
+        list.forEach(System.out::println);
+        Set<String> set = new HashSet<>();
+        Integer i = 1;
+        for (String x : list) {
+            i++;
+            set.add(x);
+        }
+        System.out.println(set);
     }
 }
